148b1b3a3fa20eb2369d0a091e99b7607a4f6b88
Paulo Vieira <paulo.cvieiradev@gmail.com>
2022-07-29 20:27:11 -0300

    Install Libs

---
+++ src/lib/apollo.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: 'https://flyby-gateway.herokuapp.com/',
+    cache: new InMemoryCache(),
+});
