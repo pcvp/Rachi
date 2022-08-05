8203d63dc23d4ac76922965484d9294e1e36541b
Paulo Vieira <paulo.cvieiradev@gmail.com>
2022-08-04 21:42:49 -0300

    Versão desktop - Formulario de contato

--- src/sections/ContatoSection.tsx
+++ src/sections/ContatoSection.tsx
@@ -4,20 +4,24 @@ import { Button } from "../components/Button";
 export default function ContatoSection() {
   return (
     <section className="bg-contato bg-cover text-2xl font-sora font-semibold ">
-      <div className="bg-contatoOpacity flex flex-col items-center text-white pt-6 pb-0 pl-6 pr-6">
-        <span className="font-semibold text-2xl">Contato</span>
-        <span className="text-gray-100 font-inter text-sm mt-4 text-center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia
-          mi quis euismod ultrices.
-        </span>
-        <input
-          className="mt-6 bg-transparent border-b-2 font-inter text-sm w-full py-2 placeholder:text-white focus:outline-none focus:outline-transparent  "
-          placeholder="Seu melhor email"
-        />
-        <Button
-          texto="Enviar"
-          className="mt-9 mb-9 w-full shadow-sm shadow-blue-900"
-        ></Button>
+      <div className="bg-contatoOpacity  text-white pt-6 pb-0 pl-6 pr-6">
+        <div className="flex flex-col items-center md:max-w-[510px] md:mx-auto ">
+          <span className="font-semibold text-2xl">Contato</span>
+          <span className="text-gray-100 font-inter text-sm mt-4 text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            lacinia mi quis euismod ultrices.
+          </span>
+          <div className="flex flex-col md:flex-row md:my-9 md:w-[500px] md:gap-10">
+            <input
+              className="mt-6 bg-transparent border-b-2 font-inter text-sm w-full placeholder:text-white focus:outline-none focus:outline-transparent  "
+              placeholder="Seu melhor email"
+            />
+            <Button
+              texto="Enviar"
+              className="my-9 w-full shadow-sm shadow-blue-900 md:my-0 md:w-[200px]"
+            ></Button>
+          </div>
+        </div>
       </div>
     </section>
   );
